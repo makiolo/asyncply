294efd5b93b908b1d41d611935cf3bb87cf5b9ce
Ricardo <makiolo@gmail.com>
2016-11-10 12:23:13 +0100

    Update test_parallel_fes.cpp

--- tests/test_parallel_fes.cpp
+++ tests/test_parallel_fes.cpp
@@ -5,90 +5,82 @@
 #include <atomic>
 #include <assert.h>
 #include <fes/async_fast.h>
+#include <gtest/gtest.h>
 #include "../parallel.h"
 #include "../task.h"
 #include "../algorithm.h"
 
-int main(int, const char**)
+class ParallelFesTest : testing::Test
 {
-	try
-	{
-		std::atomic<int> total;
-		{
-			total = 0;
-			fes::async_fast<int> channel;
-			channel.connect([&total](int data) {
-				total += data;
-			});
-			asyncply::parallel_sync(
-				[&]()
-				{
-					// consumer
-					for(int i=0; i<10; ++i)
-						channel.get();
-				},
-				[&]()
-				{
-					// consumer
-					for(int i=0; i<10; ++i)
-						channel.get();
-				},
-				[&]()
-				{
-					// consumer
-					for(int i=0; i<10; ++i)
-						channel.get();
-				},
-				[&]()
-				{
-					// producer
-					std::vector<int> v;
-					for(int i=0; i<10; ++i)
-					{
-						v.emplace_back(i*2);
-					}
-					asyncply::for_each_sync(v.begin(), v.end(), [&](int data){
-						channel(data);
-					});
-				},
-				[&]()
-				{
-					// producer
-					std::vector<int> v;
-					for(int i=0; i<10; ++i)
-					{
-						v.emplace_back(i*3);
-					}
-					asyncply::for_each_sync(v.begin(), v.end(), [&](int data){
-						channel(data);
-					});
-				},
-				[&]()
-				{
-					// producer
-					std::vector<int> v;
-					for(int i=0; i<10; ++i)
-					{
-						v.emplace_back(i*4);
-					}
-					asyncply::for_each_sync(v.begin(), v.end(), [&](int data){
-						channel(data);
-					});
-				});
-		}
-		std::cout << "total " << total << std::endl;
-		if (total != 405)
-		{
-			std::cout << "invalid total " << total << std::endl;
-			throw std::exception();
-		}
-	}
-	catch (std::exception& e)
-	{
-		std::cout << "general exception " << e.what() << std::endl;
-		return 1;
-	}
 
+};
+
+TEST(ParallelFesTest, Test1)
+{
+	std::atomic<int> total;
+	{
+		total = 0;
+		fes::async_fast<int> channel;
+		channel.connect([&total](int data) {
+			total += data;
+		});
+		asyncply::parallel_sync(
+			[&]()
+			{
+				// consumer
+				for(int i=0; i<10; ++i)
+					channel.get();
+			},
+			[&]()
+			{
+				// consumer
+				for(int i=0; i<10; ++i)
+					channel.get();
+			},
+			[&]()
+			{
+				// consumer
+				for(int i=0; i<10; ++i)
+					channel.get();
+			},
+			[&]()
+			{
+				// producer
+				std::vector<int> v;
+				for(int i=0; i<10; ++i)
+				{
+					v.emplace_back(i*2);
+				}
+				asyncply::for_each_sync(v.begin(), v.end(), [&](int data){
+					channel(data);
+				});
+			},
+			[&]()
+			{
+				// producer
+				std::vector<int> v;
+				for(int i=0; i<10; ++i)
+				{
+					v.emplace_back(i*3);
+				}
+				asyncply::for_each_sync(v.begin(), v.end(), [&](int data){
+					channel(data);
+				});
+			},
+			[&]()
+			{
+				// producer
+				std::vector<int> v;
+				for(int i=0; i<10; ++i)
+				{
+					v.emplace_back(i*4);
+				}
+				asyncply::for_each_sync(v.begin(), v.end(), [&](int data){
+					channel(data);
+				});
+			});
+	}
+	ASSERT_EQ(total, 405);
 	return 0;
 }
 
