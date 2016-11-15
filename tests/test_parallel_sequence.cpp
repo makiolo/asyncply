d3ead71795006ea4e841d32781d12b5dd5c1023c
Ricardo <makiolo@gmail.com>
2016-11-15 10:57:34 +0100

    Update test_parallel_sequence.cpp

--- tests/test_parallel_sequence.cpp
+++ tests/test_parallel_sequence.cpp
@@ -4,67 +4,53 @@
 #include "../parallel.h"
 #include "../sequence.h"
 #include "../task.h"
+#include <gtest/gtest.h>
 
-int main(int, const char**)
+class ParallelSequenceTest : testing::Test
 {
-	try
-	{
-		for (int i = 0; i < 100; ++i)
-		{
-			std::vector<asyncply::task_t< asyncply::task_t<double> > > vjobs;
-			asyncply::_parallel(vjobs,
-				[&]()
-				{
-					return asyncply::sequence(7.0,
-						[](double data)
-						{
-							return data + 3.0;
-						},
-						[](double data)
-						{
-							return data + 4.0;
-						});
-				},
-				[&]()
-				{
-					return asyncply::sequence(9.0,
-						[](double data)
-						{
-							return data + 5.0;
-						},
-						[](double data)
-						{
-							return data + 4.0;
-						});
-				});
-			double aggregation = 0.0;
-			for (auto& job : vjobs)
-			{
-				try
-				{
-					// double get get
-					double partial = job->get()->get();
-					aggregation += partial;
-				}
-				catch (std::exception& e)
-				{
-					std::cout << "exception: " << e.what() << std::endl;
-				}
-			}
-			if (std::abs(aggregation - 32.0) > 1e-3)
-			{
-				std::cout << "invalid total " << aggregation << std::endl;
-				throw std::exception();
-			}
-			// std::cout << "aggregation = " << aggregation << std::endl;
-		}
-		std::cout << "result ok" << std::endl;
-	}
-	catch (std::exception& e)
-	{
-		std::cout << "general exception " << e.what() << std::endl;
-		return 1;
-	}
-	return 0;
-}
 
+};
+
+TEST(ParallelSequenceTest, Test1)
+{
+	std::vector<asyncply::task_t< asyncply::task_t<double> > > vjobs;
+	asyncply::_parallel(vjobs,
+		[&]()
+		{
+			return asyncply::sequence(7.0,
+				[](double data)
+				{
+					return data + 3.0;
+				},
+				[](double data)
+				{
+					return data + 4.0;
+				});
+		},
+		[&]()
+		{
+			return asyncply::sequence(9.0,
+				[](double data)
+				{
+					return data + 5.0;
+				},
+				[](double data)
+				{
+					return data + 4.0;
+				});
+		});
+	double aggregation = 0.0;
+	for (auto& job : vjobs)
+	{
+		try
+		{
+			double partial = job->get()->get();
+			aggregation += partial;
+		}
+		catch (std::exception& e)
+		{
+			std::cout << "exception: " << e.what() << std::endl;
+		}
+	}
+	ASSERT_EQ(aggregation, 32.0);
+}
