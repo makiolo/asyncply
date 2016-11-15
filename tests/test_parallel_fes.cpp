50b92b075c7b7603a77213d41366913b1f0e60eb
Ricardo <makiolo@gmail.com>
2016-11-15 11:27:38 +0100

    Update test_parallel_fes.cpp

--- tests/test_parallel_fes.cpp
+++ tests/test_parallel_fes.cpp
@@ -9,6 +9,42 @@
 #include "../task.h"
 #include "../algorithm.h"
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+class foo
+{
+public:
+	explicit foo() { std::cout << "constructor empty" << std::endl; }
+	explicit foo(const std::string& data) : _data(data) { std::cout << "constructor with parms" << std::endl; }
+	virtual ~foo() { std::cout << "destructor" << std::endl; }
+	
+	foo(const foo& other) : _data(other._data) { std::cout << "constructor copia" << std::endl; }
+	foo& operator=(const foo& other) 
+	{
+		std::cout << "operador asignación" << std::endl;
+		foo(other).swap(*this);
+		return *this;
+	}
+
+	foo(foo&& other) : _data(std::move(other._data)) { std::cout << "constructor de movimiento" << std::endl; }
+
+	foo& operator=(foo&& other)
+	{
+		std::cout << "operador asignacion de movimiento" << std::endl;
+		foo(std::move(other)).swap(*this);
+		return *this;
+	}
+
+	void swap(foo& other)
+	{
+		std::cout << "swap" << std::endl;
+		using std::swap;
+		swap(_data, other._data);
+	}
+
+private:
+	std::string _data;
+};
 
 class ParallelFesTest : testing::Test
 {
@@ -81,4 +117,16 @@ TEST(ParallelFesTest, Test1)
 			});
 	}
 	ASSERT_EQ(total, 405);
+}
+
+TEST(ParallelFesTest, Test2)
+{
+	fes::async_fast<foo> channel;
+	channel.connect([](const foo& f) {
+		std::cout << "received foo" << std::endl;
+	});
+	// send foo
+	channel(foo("bar"));
+	// recv foo
+	channel.get();
 }
