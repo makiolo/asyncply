2a024d412d84856ac8ce9e5f93f1374e66667178
Ricardo <makiolo@gmail.com>
2016-11-17 09:51:40 +0100

    Update test_parallel_fes.cpp

--- tests/test_parallel_fes.cpp
+++ tests/test_parallel_fes.cpp
@@ -49,71 +49,57 @@ private:
 
 class ParallelFesTest : testing::Test { };
 
-TEST(DISABLED_ParallelFesTest, Test1)
+TEST(ParallelFesTest, Test1)
 {
 	std::atomic<int> total;
-	{
-		total = 0;
-		fes::async_fast<int> channel;
-		channel.connect([&total](int data) {
-			total += data;
+	total = 0;
+	fes::async_fast<int> channel;
+	channel.connect([&total](int data) {
+		total += data;
+	});
+	asyncply::parallel_sync(
+		[&]()
+		{
+			// consumer
+			for(int i=0; i<10; ++i)
+				channel.get();
+		},
+		[&]()
+		{
+			// consumer
+			for(int i=0; i<10; ++i)
+				channel.get();
+		},
+		[&]()
+		{
+			// consumer
+			for(int i=0; i<10; ++i)
+				channel.get();
+		},
+		[&]()
+		{
+			// producer
+			for(int i=0; i<10; ++i)
+			{
+				channel(i*2);
+			}
+		},
+		[&]()
+		{
+			// producer
+			for(int i=0; i<10; ++i)
+			{
+				channel(i*3);
+			}
+		},
+		[&]()
+		{
+			// producer
+			for(int i=0; i<10; ++i)
+			{
+				channel(i*4);
+			}
 		});
-		asyncply::parallel_sync(
-			[&]()
-			{
-				// consumer
-				for(int i=0; i<10; ++i)
-					channel.get();
-			},
-			[&]()
-			{
-				// consumer
-				for(int i=0; i<10; ++i)
-					channel.get();
-			},
-			[&]()
-			{
-				// consumer
-				for(int i=0; i<10; ++i)
-					channel.get();
-			},
-			[&]()
-			{
-				// producer
-				std::vector<int> v;
-				for(int i=0; i<10; ++i)
-				{
-					v.emplace_back(i*2);
-				}
-				asyncply::for_each_sync(v.begin(), v.end(), [&](int data){
-					channel(data);
-				});
-			},
-			[&]()
-			{
-				// producer
-				std::vector<int> v;
-				for(int i=0; i<10; ++i)
-				{
-					v.emplace_back(i*3);
-				}
-				asyncply::for_each_sync(v.begin(), v.end(), [&](int data){
-					channel(data);
-				});
-			},
-			[&]()
-			{
-				// producer
-				std::vector<int> v;
-				for(int i=0; i<10; ++i)
-				{
-					v.emplace_back(i*4);
-				}
-				asyncply::for_each_sync(v.begin(), v.end(), [&](int data){
-					channel(data);
-				});
-			});
-	}
 	ASSERT_EQ(total, 405);
 }
 
