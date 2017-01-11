b3b4b512594c65d8742e71ca9b8cd31e977144de
Ricardo <makiolo@gmail.com>
2017-01-11 17:39:54 +0100

    Update task.h

--- task.h
+++ task.h
@@ -13,13 +13,10 @@ template <typename R>
 class task : public std::enable_shared_from_this<task<R> >
 {
 public:
-	using func = std::function<R()>;
-
 	template <typename Function, typename ... Args>
 	task(Function&& f, Args&& ... args)
 		: _result( asyncply::_async(std::forward<Function>(f), std::forward<Args>(args)...) )
 	{ ; }
-
 	~task() { ; }
 
 	task(const task&) = delete;
@@ -50,9 +47,41 @@ public:
 		);
 	}
 
-	R get()
+	typename std::enable_if<(!std::is_void<R>::value), R>::type get()
 	{
 		return _result.get();
+	}
+	
+	typename std::enable_if<(!std::is_void<R>::value), R&>::type get()
+	{
+		return _result.get();
+	}
+	
+	typename std::enable_if<(std::is_void<R>::value), void>::type get()
+	{
+		_result.get();
+	}
+	
+	bool valid() const
+	{
+		return _result.valid();
+	}
+	
+	void wait() const
+	{
+		_result.wait();
+	}
+	
+	template< class Rep, class Period >
+	std::future_status wait_for( const std::chrono::duration<Rep,Period>& timeout_duration ) const
+	{
+		return _reseult.wait_for(timeout_duration);
+	}
+	
+	template< class Clock, class Duration >
+	std::future_status wait_until( const std::chrono::time_point<Clock,Duration>& timeout_time ) const
+	{
+		return _result.wait_until(timeout_time);
 	}
 
 protected:
