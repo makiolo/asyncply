0522f2127c3e3cd46ae1539d1b9ae8e050c5b060
Ricardo <makiolo@gmail.com>
2017-01-11 19:20:01 +0100

    Update task.h

--- task.h
+++ task.h
@@ -22,25 +22,12 @@ public:
 	task& operator=(const task&) = delete;
 
 	template <typename Function>
-	task_t<R> then(typename std::enable_if<(!std::is_void<typename std::result_of<Function(R)>::type>::value), Function>::type&& post_method)
+	task_t<R> then(Function&& post_method)
 	{
 		task_t<R> this_task = this->shared_from_this();
 		return asyncply::async(
 					[this_task](Function&& post_method){
 						return post_method(this_task->get());
-					},
-					std::forward<Function>(post_method)
-		);
-	}
-
-	template <typename Function>
-	task_t<R> then(typename std::enable_if<(std::is_void<typename std::result_of<Function()>::type>::value), Function>::type&& post_method)
-	{
-		task_t<R> this_task = this->shared_from_this();
-		return asyncply::async(
-					[this_task](Function&& post_method){
-						this_task->get();
-						post_method();
 					},
 					std::forward<Function>(post_method)
 		);
@@ -75,6 +62,63 @@ public:
 
 protected:
 	std::future<R> _result;
+};
+
+template <>
+class task<void> : public std::enable_shared_from_this<task<void> >
+{
+public:
+	template <typename Function, typename ... Args>
+	task(Function&& f, Args&& ... args)
+		: _result( asyncply::_async(std::forward<Function>(f), std::forward<Args>(args)...) ) { ; }
+	~task() { ; }
+
+	task(const task&) = delete;
+	task& operator=(const task&) = delete;
+
+	template <typename Function>
+	//task_t<R> then(typename std::enable_if<(std::is_void<typename std::result_of<Function()>::type>::value), Function>::type&& post_method)
+	task_t<void> then(Function&& post_method)
+	{
+		task_t<void> this_task = this->shared_from_this();
+		return asyncply::async(
+					[this_task](Function&& post_method){
+						this_task->get();
+						post_method();
+					},
+					std::forward<Function>(post_method)
+		);
+	}
+
+	void get()
+	{
+		_result.get();
+	}
+
+	bool valid() const
+	{
+		return _result.valid();
+	}
+
+	void wait() const
+	{
+		_result.wait();
+	}
+
+	template< class Rep, class Period >
+	std::future_status wait_for( const std::chrono::duration<Rep,Period>& timeout_duration ) const
+	{
+		return _result.wait_for(timeout_duration);
+	}
+
+	template< class Clock, class Duration >
+	std::future_status wait_until( const std::chrono::time_point<Clock,Duration>& timeout_time ) const
+	{
+		return _result.wait_until(timeout_time);
+	}
+
+protected:
+	std::future<void> _result;
 };
 
 }
