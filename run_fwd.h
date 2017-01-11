be610aa8f8c8946527d037e231897fe53972fc67
Ricardo <makiolo@gmail.com>
2017-01-11 15:35:39 +0100

    Update run_fwd.h

--- run_fwd.h
+++ run_fwd.h
@@ -9,23 +9,28 @@
 
 namespace asyncply {
 
+// functor_type
 template <typename Function>
 using return_of_functor = typename std::result_of<Function()>::type;
 
+// task_functor
 template <typename Function>
 using task_of_functor = asyncply::task< return_of_functor<Function> >;
 
+// future_functor
+template <typename Function>
+using future_of_functor = std::future< return_of_functor<Function> >;
+  
+// shared_task
 template <typename Type>
 using task_t = std::shared_ptr< asyncply::task<Type> >;
 
+// shared_task_functor
 template <typename Function>
 using shared_task = std::shared_ptr<task_of_functor<Function>>;
-
-template <typename Function>
-shared_task<Function> async(Function&& f);
-
-template <typename Function>
-using future_of_functor = std::future< return_of_functor<Function> >;
+  
+template <typename Function, typename ... Args>
+shared_task<Function> async(Function&& f, Args&& ... args);
 
 }
 
