cd59b40481b2d50cb8e05911aaa60fa43c88e5cf
Ricardo <makiolo@gmail.com>
2017-01-12 16:38:04 +0100

    Update async.h

--- async.h
+++ async.h
@@ -2,23 +2,15 @@
 #define _ASYNC_H_
 
 #include "run_fwd.h"
-#include "thread-pool-cpp/thread_pool/thread_pool.hpp"
 
 namespace asyncply {
 
 template <typename Function, typename ... Args>
-future_of_functor<Function> _async(Function&& f, Args&& ... data)
+future_of_functor<Function, Args...> _async(Function&& f, Args&& ... data)
 {
-#if 0
-	// not working well :(
-	static ThreadPool __pool;
-	return __pool.process(std::forward<Function>(f), std::forward<Args>(data)...);
-#else
 	return std::async(std::launch::async, std::forward<Function>(f), std::forward<Args>(data)...);
-#endif
 }
 
 }
 
 #endif
-
