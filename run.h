a26febe0773e338831a4e4ac14d070e02556d06a
Ricardo <makiolo@gmail.com>
2017-01-17 17:22:42 +0100

    Update run.h

--- run.h
+++ run.h
@@ -337,16 +337,16 @@ namespace asyncply
         template <typename Func, typename... Args>
         inline auto submitJob(Func&& func, Args&&... args)
         {
-            return getThreadPool().submit(std::forward<Func>(func), std::forward<Args>(args)...);
+            return DefaultThreadPool::getThreadPool().submit(std::forward<Func>(func), std::forward<Args>(args)...);
         }
  
         /**
          * Get the default thread pool for the application.
          * This pool is created with std::thread::hardware_concurrency() - 1 threads.
          */
-        inline ThreadPool& getThreadPool(void)
+        inline asyncply::ThreadPool& getThreadPool(void)
         {
-            static ThreadPool defaultPool;
+            static asyncply::ThreadPool defaultPool;
             return defaultPool;
         }
     }
