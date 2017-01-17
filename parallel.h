9acfe378aac37a3e2ef1933f409c694ec3c31469
Ricardo <makiolo@gmail.com>
2017-01-17 12:51:56 +0100

    Update parallel.h

--- parallel.h
+++ parallel.h
@@ -8,11 +8,52 @@
 
 namespace asyncply {
 
-template <typename Function>
+/*
+template<class Function>
+struct is_task : std::false_type {};
+	
+template<class Function>
+struct is_task<std::shared_ptr<> > : std::true_type {};
+*/
+	
+template<typename T>
+class is_task
+{
+	typedef char(&yes)[2];
+
+	template<typename> struct Exists;
+
+	template<typename V>
+	static yes CheckMember(Exists<decltype(&V::element_type::return_type)>*);
+	template<typename>
+	static char CheckMember(...);
+
+public:
+	static const bool value = (sizeof(CheckMember<T>(0)) == sizeof(yes));
+};
+	
+template <typename Function,
+		class = typename std::enable_if<
+			(is_task<typename std::result_of<Function()>::type>::value)
+		>::type
+	>
 auto wtf_return_type_is_this()
 {
 	return [](){
 		using ret_t = typename std::result_of<Function()>::type::element_type::return_type;
+		return ret_t();
+	};
+}
+
+template <typename Function,
+		class = typename std::enable_if<
+			(!is_task<typename std::result_of<Function()>::type>::value)
+		>::type
+	>
+auto wtf_return_type_is_this()
+{
+	return [](){
+		using ret_t = typename std::result_of<Function()>::type;
 		return ret_t();
 	};
 }
@@ -37,7 +78,7 @@ template <  typename Function,
 			class = typename std::enable_if<
 				(!std::is_arithmetic<typename std::result_of<Function()>::type>::value) &&
 				(!std::is_same<typename std::result_of<Function()>::type, bool>::value) &&
-				(!std::is_same<typename std::result_of<Function()>::type, task_t< decltype(wtf_return_type_is_this<Function>()) > >::value)
+				(!std::is_same<typename std::result_of<Function()>::type, task_t< double > >::value)
 			>::type
 	>
 std::vector<typename std::result_of<Function()>::type> aggregation(Container&& vf)
@@ -54,7 +95,7 @@ template <  typename Function,
 			class = typename std::enable_if<
 				(std::is_arithmetic<typename std::result_of<Function()>::type>::value) &&
 				(!std::is_same<typename std::result_of<Function()>::type, bool>::value) &&
-				(!std::is_same<typename std::result_of<Function()>::type, task_t< decltype(wtf_return_type_is_this<Function>()) > >::value)
+				(!std::is_same<typename std::result_of<Function()>::type, task_t< double > >::value)
 			>::type
 	>
 typename std::result_of<Function()>::type aggregation(Container&& vf)
@@ -71,7 +112,7 @@ template <  typename Function,
 			class = typename std::enable_if<
 				(!std::is_arithmetic<typename std::result_of<Function()>::type>::value) &&
 				(std::is_same<typename std::result_of<Function()>::type, bool>::value) &&
-				(!std::is_same<typename std::result_of<Function()>::type, task_t< decltype(wtf_return_type_is_this<Function>()) > >::value)
+				(!std::is_same<typename std::result_of<Function()>::type, task_t< double > >::value)
 			>::type
 	>
 bool aggregation(Container&& vf)
@@ -88,7 +129,7 @@ template <  typename Function,
 			class = typename std::enable_if<
 				(!std::is_arithmetic<typename std::result_of<Function()>::type>::value) &&
 				(!std::is_same<typename std::result_of<Function()>::type, bool>::value) &&
-				(std::is_same<typename std::result_of<Function()>::type, task_t< decltype(wtf_return_type_is_this<Function>()) > >::value)
+				(std::is_same<typename std::result_of<Function()>::type, task_t< double > >::value)
 			>::type
 	>
 // TODO: only works with double
