396e828612c2f59c1544edf7a7cba0873b416a54
Ricardo <makiolo@gmail.com>
2017-01-18 12:27:03 +0100

    Update run_fwd.h

--- run_fwd.h
+++ run_fwd.h
@@ -33,8 +33,8 @@ template <typename Function, typename ... Args>
 using shared_task = std::shared_ptr< task_of_functor<Function, Args...> >;
 
 // submitJob
-template <typename Function, typename... Args>
-inline future_of_functor<Func, Args...> submitJob(Function&& func, Args&&... args);
+template <typename Func, typename... Args>
+inline future_of_functor<Func, Args...> submitJob(Func&& func, Args&&... args);
   
 // async
 template <typename Function, typename ... Args>
