2c65d9ed951d57560adeaa0ab7bbf19369c3fc4e
Ricardo <makiolo@gmail.com>
2017-01-19 11:55:40 +0100

    Update async.h

--- async.h
+++ async.h
@@ -8,7 +8,7 @@ namespace asyncply {
 template <typename Function, typename ... Args>
 future_of_functor<Function, Args...> _async(Function&& f, Args&& ... data)
 {
-	return asyncply::submitJob(std::forward<Function>(f), std::forward<Args>(data)...);
+	return asyncply::__async(std::forward<Function>(f), std::forward<Args>(data)...);
 }
 
 }
