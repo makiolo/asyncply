adae0468f92ddd4c8ff988eee5194377db7dad56
Ricardo <makiolo@gmail.com>
2017-01-19 16:24:21 +0100

    Update sequence.h

--- sequence.h
+++ sequence.h
@@ -7,22 +7,22 @@
 namespace asyncply {
 
 template <typename Data, typename Function>
-void __sequence(task_t<Data>& task, Data data, Function&& f)
+void __sequence(task_t<Data>& task, Data&& data, Function&& f)
 {
 	if(!task)
 	{
 		task = asyncply::async( 	
-			[f = std::move(f)](Data d) -> Data
+			[f = std::move(f)](Data&& d) -> Data
 			{
 				return f(d);
-			}, data
+			}, std::forward<Data>(data)
 		);
 	}
 	else
 	{
 		task = task->then(
 			std::bind(
-				[f = std::move(f)](Data d) -> Data
+				[f = std::move(f)](Data&& d) -> Data
 				{
 					return f(d);
 				}, std::placeholders::_1
@@ -32,35 +32,35 @@ void __sequence(task_t<Data>& task, Data data, Function&& f)
 }
 	
 template <typename Data, typename Function>
-void _sequence(task_t<Data>& task, Data data, Function&& f)
+void _sequence(task_t<Data>& task, Data&& data, Function&& f)
 {
-	__sequence(task, data, std::forward<Function>(f));
+	asyncply::__sequence(task, std::forward<Data>(data), std::forward<Function>(f));
 }
 
 template <typename Data, typename Function, typename... Functions>
-void _sequence(task_t<Data>& task, Data data, Function&& f, Functions&& ... fs)
+void _sequence(task_t<Data>& task, Data&& data, Function&& f, Functions&& ... fs)
 {
-	__sequence(task, data, std::forward<Function>(f));
-	asyncply::_sequence<Data>(task, data, std::forward<Functions>(fs)...);
+	asyncply::__sequence(task, std::forward<Data>(data), std::forward<Function>(f));
+	asyncply::_sequence(task, std::forward<Data>(data), std::forward<Functions>(fs)...);
 }
 
 template <typename Data, typename... Functions>
-Data sequence_sync(Data data, Functions&&... fs)
+Data sequence_sync(Data&& data, Functions&&... fs)
 {
 	task_t<Data> task;
-	asyncply::_sequence<Data>(task, data, std::forward<Functions>(fs)...);
+	asyncply::_sequence(task, std::forward<Data>(data), std::forward<Functions>(fs)...);
 	return task->get();
 }
 
 template <typename Data, typename... Functions>
-task_t<Data> sequence(Data data, Functions&&... fs)
+task_t<Data> sequence(Data&& data, Functions&&... fs)
 {
 	return asyncply::async(
-		[](Data d, Functions&&... fs) -> Data
+		[](Data&& d, Functions&&... fs) -> Data
 		{
 			return asyncply::sequence_sync(d, std::forward<Functions>(fs)...);
 		},
-		data, std::forward<Functions>(fs)...
+		std::forward<Data>(data), std::forward<Functions>(fs)...
 	);
 }
 
