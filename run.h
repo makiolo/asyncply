448eeed0a41f94c7da505ed4b6bcf7dc642be6b0
Ricardo <makiolo@gmail.com>
2017-01-19 12:47:08 +0100

    Update run.h

--- run.h
+++ run.h
@@ -252,8 +252,8 @@ auto submit(Func&& func, Args&&... args) -> future_of_functor<Func, Args...>
 	using PackagedTask = std::packaged_task< result_type<Func, Args...>() >;
 	using TaskType = ThreadTask<PackagedTask>;
 
-	static_assert(std::is_same< result_type<Func, Args...>, decltype(boundTask()) >::value, "error in is_same");
-	static_assert(std::is_same< future_of_functor<Func, Args...>, TaskFuture< result_type<Func, Args...> > >::value, "error in is_same");
+	//static_assert(std::is_same< result_type<Func, Args...>, decltype(boundTask()) >::value, "error in is_same");
+	//static_assert(std::is_same< future_of_functor<Func, Args...>, TaskFuture< result_type<Func, Args...> > >::value, "error in is_same");
 	
 	PackagedTask task( std::move(boundTask) );
 	future_of_functor<Func, Args...> result( task.get_future() );
