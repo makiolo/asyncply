402d173867965136d19431621c29494846991b2e
Ricardo <makiolo@gmail.com>
2017-03-09 19:12:40 +0100

    Update run_fwd.h

--- run_fwd.h
+++ run_fwd.h
@@ -11,9 +11,10 @@ namespace asyncply {
 // forward
 template <typename T> class TaskFuture;
 class ThreadPool;
-  
+
 // thread pool
-using thread_pool = ThreadPool;
+// using thread_pool = ThreadPool;
+using thread_pool = ctpl::thread_pool;
 
 // functor_type
 template <typename Function, typename ... Args>
@@ -34,11 +35,11 @@ using task_t = std::shared_ptr< asyncply::task<Type> >;
 // shared_task_functor
 template <typename Function, typename ... Args>
 using shared_task = std::shared_ptr< task_of_functor<Function, Args...> >;
-  
+
 // __async
 template <typename Function, typename ... Args>
 auto __async(Function&& f, Args&& ... data) -> future_of_functor<Function, Args...>;
-  
+
 // async
 template <typename Function, typename ... Args>
 auto async(Function&& f, Args&& ... args) -> shared_task<Function, Args...>;
