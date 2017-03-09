f6f1c58372c6de6eea9936b8deb13f3b77f8630e
Ricardo <makiolo@gmail.com>
2017-03-09 20:13:48 +0100

    Update run.h

--- run.h
+++ run.h
@@ -168,8 +168,8 @@ namespace ctpl {
         }
 
         template<typename F, typename... Rest>
-        auto push(F && f, Rest&&... rest) ->std::future<decltype(f(0, rest...))> {
-            auto pck = std::make_shared<std::packaged_task<decltype(f(0, rest...))(int)>>(
+        auto push(F&& f, Rest&&... rest) ->std::future< result_type<F, int, Rest...> > {
+            auto pck = std::make_shared<std::packaged_task< result_type<F, int, Rest...>(int)> >(
                 std::bind(std::forward<F>(f), std::placeholders::_1, std::forward<Rest>(rest)...)
             );
 
@@ -187,8 +187,8 @@ namespace ctpl {
         // run the user's function that excepts argument int - id of the running thread. returned value is templatized
         // operator returns std::future, where the user can get the result and rethrow the catched exceptins
         template<typename F>
-        auto push(F && f) ->std::future<decltype(f(0))> {
-            auto pck = std::make_shared<std::packaged_task<decltype(f(0))(int)>>(std::forward<F>(f));
+        auto push(F&& f) -> std::future< result_type<F, int> > {
+            auto pck = std::make_shared<std::packaged_task< result_type<F, int>(int)>>(std::forward<F>(f));
 
             auto _f = new std::function<void(int id)>([pck](int id) {
                 (*pck)(id);
