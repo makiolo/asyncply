69b932fbd6eb6f08e06fef17b4df7dd238bbe10f
Ricardo <makiolo@gmail.com>
2017-03-09 19:16:56 +0100

    Update run.cpp

--- run.cpp
+++ run.cpp
@@ -5,7 +5,7 @@ namespace asyncply {
 
 asyncply::thread_pool& getThreadPool(void)
 {
-    static asyncply::thread_pool defaultPool;
+    static asyncply::thread_pool defaultPool( std::thread::hardware_concurrency() - 1 );
     return defaultPool;
 }
 
