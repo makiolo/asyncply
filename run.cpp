d6f8692036e4d990c7f616c34c169ae9903f48f3
Ricardo <makiolo@gmail.com>
2017-06-07 11:43:04 +0200

    Update run.cpp

--- run.cpp
+++ run.cpp
@@ -1,11 +1,17 @@
 #include "run.h"
 // 
 
+#ifdef _WIN32
+#define STATIC_MULTITHREAD __declspec(thread) static
+#else
+#define STATIC_MULTITHREAD static __thread
+#endif
+
 namespace asyncply {
 
 asyncply::thread_pool& getThreadPool(void)
 {
-    static asyncply::thread_pool defaultPool( std::thread::hardware_concurrency() - 1 );
+    STATIC_MULTITHREAD asyncply::thread_pool defaultPool( std::thread::hardware_concurrency() - 1 );
     return defaultPool;
 }
 
