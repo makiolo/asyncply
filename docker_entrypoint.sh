225a3c253302625c43caa4ff32204218d6c2b894
Ricardo <makiolo@MacBook-Pro-de-Ricardo.local>
2019-04-19 19:20:47 +0200

    .

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -6,13 +6,17 @@ export COMPILER="${COMPILER:-gcc}"
 export COMPILER_LIBCXX="${COMPILER_LIBCXX:-libstdc++11}"
 export COMPILER_VERSION="${COMPILER_VERSION:-7.3}"
 
-# force recompile
-# RUN conan install gtest/1.8.1@bincrafters/stable --build -s compiler=$COMPILER -s build_type=$MODE -s compiler.libcxx=$COMPILER_LIBCXX -s compiler.version=$COMPILER_VERSION
-# RUN conan install boost/1.70.0@conan/stable --build -s compiler=$COMPILER -s build_type=$MODE -s compiler.libcxx=$COMPILER_LIBCXX -s compiler.version=$COMPILER_VERSION
-# RUN conan create . npm-mas-mas/testing -s compiler=$COMPILER -s build_type=$MODE -s compiler.libcxx=$COMPILER_LIBCXX -s compiler.version=$COMPILER_VERSION -tf None
-# RUN conan upload '*' -r npm-mas-mas --all -c
+if [ "$(uname)" == "Darwin" ]; then
+	# mac
+	export COMPILER=apple-clang COMPILER_VERSION=10.0 COMPILER_LIBCXX=libc++
+fi
+
+# compile 3rd parties
+# conan install . --build missing -s compiler=$COMPILER -s build_type=$MODE -s compiler.libcxx=$COMPILER_LIBCXX -s compiler.version=$COMPILER_VERSION
+# conan create . npm-mas-mas/testing --build $PACKAGE -s compiler=$COMPILER -s build_type=$MODE -s compiler.libcxx=$COMPILER_LIBCXX -s compiler.version=$COMPILER_VERSION -tf None
+# conan upload $PACKAGE/*@npm-mas-mas/testing -r npm-mas-mas --all -c
 
 # compile only $PACKAGE
-conan create . npm-mas-mas/testing -s compiler=$COMPILER -s build_type=$MODE -s compiler.libcxx=$COMPILER_LIBCXX -s compiler.version=$COMPILER_VERSION -tf None
+conan create . npm-mas-mas/testing --build $PACKAGE -s compiler=$COMPILER -s build_type=$MODE -s compiler.libcxx=$COMPILER_LIBCXX -s compiler.version=$COMPILER_VERSION -tf None
 conan upload $PACKAGE/*@npm-mas-mas/testing -r npm-mas-mas --all -c
 
